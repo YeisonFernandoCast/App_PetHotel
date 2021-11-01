e5e1edd3b307bd2657ace887f2e8fd90eda9067c
YEISON <yefer08@gmail.com>
2021-11-01 14:29:07 -0500

    Commit03 - pantalla de registro

--- app/src/main/java/com/faroti/cuidotumascota/MainActivity.java
+++ app/src/main/java/com/faroti/cuidotumascota/MainActivity.java
@@ -3,12 +3,20 @@ package com.faroti.cuidotumascota;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
