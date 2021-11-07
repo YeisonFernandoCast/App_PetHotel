f4c135d7db195dcd432acb9f1a5d5a4f22f948ac
Johan <johanseb199@gmail.com>
2021-11-07 16:03:20 -0500

    iniciando eventos

--- app/src/main/java/com/faroti/petshotel/LaunchRegister.java
+++ app/src/main/java/com/faroti/petshotel/LaunchRegister.java
@@ -1,14 +1,32 @@
 package com.faroti.petshotel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LaunchRegister extends AppCompatActivity {
+
+
+    AppCompatButton buttonRegister;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_register);
+
+
+        buttonRegister = findViewById(R.id.button_register_launch);
+        buttonRegister.setOnClickListener((evt) -> {
+            Toast.makeText(this, "Registro Completo", Toast.LENGTH_SHORT).show();
+        });
+
+
     }
 }
--- app/src/main/java/com/faroti/petshotel/RegisterActivity.java
+++ app/src/main/java/com/faroti/petshotel/RegisterActivity.java
@@ -1,14 +1,44 @@
 package com.faroti.petshotel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    ImageView ivLogo;
+    TextInputEditText EmailRegister;
+    TextInputEditText passwordRegister;
+
+    AppCompatButton buttonRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        ivLogo = findViewById(R.id.logoRegister);
+
+        EmailRegister = findViewById(R.id.editText_emailRegister);
+        EmailRegister.setText("example@gmail.com");
+
+        passwordRegister = findViewById(R.id.editText_passwordRegister);
+        passwordRegister.setText("Abcd_1234");
+
+        buttonRegister = findViewById(R.id.button_register_launch);
+        buttonRegister.setOnClickListener((evt) -> {
+            EmailRegister.setText("");
+            Toast.makeText(this, "Registro Completo", Toast.LENGTH_SHORT).show();
+        });
+
+
     }
 }
