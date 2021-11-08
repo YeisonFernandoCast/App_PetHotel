c08c7909f258d083fed9a2c8226176d474c5cc69
JP <jfpacheco11@gmail.com>
2021-11-07 19:30:39 -0500

    Java variables

--- app/src/main/java/com/faroti/petshotel/InfoContacto.java
+++ app/src/main/java/com/faroti/petshotel/InfoContacto.java
@@ -3,12 +3,34 @@ package com.faroti.petshotel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class InfoContacto extends AppCompatActivity {
+
+    ImageView iv_inicio;
+    ImageView iv_salir;
+    TextInputLayout tilbuscar;
+    TextInputEditText etbuscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_contacto);
+
+        initUI();
     }
+
+    private void initUI() {
+        iv_inicio = findViewById(R.id.iv_inicio);
+        iv_salir = findViewById(R.id.iv_salir);
+
+        tilbuscar= findViewById(R.id.til_buscar);
+        etbuscar = findViewById(R.id.et_buscar);
+
+
+
+
 }
