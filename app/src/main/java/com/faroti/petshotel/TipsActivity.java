714e50a39c7b63e704d86f3991da130136b9607e
Johan <johanseb199@gmail.com>
2021-11-10 00:30:39 -0500

    Registro and tips running

--- app/src/main/java/com/faroti/petshotel/TipsActivity.java
+++ app/src/main/java/com/faroti/petshotel/TipsActivity.java
@@ -2,10 +2,9 @@ package com.faroti.petshotel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 
 public class TipsActivity extends AppCompatActivity {
 
