f97a3459bab18c0ef55c17f6de5a1a43391db8cd
Johan <johanseb199@gmail.com>
2021-11-10 01:19:29 -0500

    tips,register,contact running

---
+++ app/src/main/java/com/faroti/petshotel/InfoContact.java
@@ -0,0 +1,14 @@
+package com.faroti.petshotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_contact);
+    }
+}
