7e17d68e62245d16f4d8ccacfec21b456039402c
Johan <johanseb199@gmail.com>
2021-11-12 08:40:59 -0500

    full screen en codiogo prueba

--- app/src/main/java/com/faroti/petshotel/union_base_activity.java
+++ app/src/main/java/com/faroti/petshotel/union_base_activity.java
@@ -2,10 +2,12 @@ package com.faroti.petshotel;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+
 import com.bumptech.glide.Glide;
 
 
@@ -24,23 +26,51 @@ public class union_base_activity extends AppCompatActivity {
                     WindowManager.LayoutParams.FLAG_FULLSCREEN); //TODO -- no sirve con appcompatactivity
                                                                  // se utilizan con extend Activity
         }*/
-/*
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN); */ //TODO -- este tampoco
 
         setContentView(R.layout.activity_union_base);
 
-        background.findViewById(R.id.background);
+        background = findViewById(R.id.background);
 
-        Glide.with(this) // TODO -- revisar esto
+        Glide.with(this)
                 .load(R.mipmap.background)
                 .centerCrop()
                 .into(background);
-
+        hideSystemUI();
         initIU();
 
     }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                 | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                 | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        // Set the content to appear under the system bars so that the
+        // content doesn't resize when the system bars hide and show.
+
+
+        // Hide the nav bar and status bar
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private void initIU() {
 
