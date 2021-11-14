732d378c4351097c01a128030730148eb9f987fc
YEISON <yefer08@gmail.com>
2021-11-14 11:25:57 -0500

    Unificación de pantallas TIPS botón de regreso e imágenes

--- app/src/main/java/com/faroti/petshotel/ActivityFive.java
+++ app/src/main/java/com/faroti/petshotel/ActivityFive.java
@@ -1,31 +1,26 @@
 package com.faroti.petshotel;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 public class ActivityFive extends AppCompatActivity {
 
-    FloatingActionButton fab;
+    AppCompatButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_five);
-        fab=(FloatingActionButton) findViewById(R.id.fab_back);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), TipsActivity.class);
-                startActivity(intent);
 
-            }
-        });
+        fab = findViewById(R.id.fab_back);
+        fab.setOnClickListener((evt) -> onClickBack());
+    }
+
+    public void onClickBack(){
+        Intent intent = new Intent(this, TipsActivity.class);
+        startActivity(intent);
     }
 }
