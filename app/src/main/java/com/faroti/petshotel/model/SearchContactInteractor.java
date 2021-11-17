359baf20b6c83b32eb21e40e789c6cec5c871ada
JP <jfpacheco11@gmail.com>
2021-11-17 07:33:30 -0500

    Recicler 80%

--- app/src/main/java/com/faroti/petshotel/model/SearchContactInteractor.java
+++ app/src/main/java/com/faroti/petshotel/model/SearchContactInteractor.java
@@ -3,8 +3,22 @@ package com.faroti.petshotel.model;
 import com.faroti.petshotel.mvp.SearchContactMVP;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SearchContactInteractor implements SearchContactMVP.Model {
+
+    private List<SearchContactMVP.SearchContactInfo> data;
+
+    public SearchContactInteractor(){
+        data = Arrays.asList(
+                new SearchContactMVP.SearchContactInfo("Jose Pacheco", "Cra 23 # 45-67"),
+                new SearchContactMVP.SearchContactInfo("Johan M", "Cra 23 # 45-67"),
+                new SearchContactMVP.SearchContactInfo("Yeison Castaño", "Cra 23 # 45-67"),
+                new SearchContactMVP.SearchContactInfo("Rodrigo Estupiñán", "Cra 23 # 45-67"),
+                new SearchContactMVP.SearchContactInfo("Francisco J", "Cra 23 # 45-67")
+        );
+    }
 
     @Override
     public void loadSearchContact(LoadSearchContactCallback callback) {
@@ -13,6 +27,6 @@ public class SearchContactInteractor implements SearchContactMVP.Model {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        callback.showSearchContactInfo(new ArrayList<>());
+        callback.showSearchContactInfo(this.data);
     }
 }
