36e6754bce801321d6ebb49489034d1d2434529e
JP <jfpacheco11@gmail.com>
2021-11-21 20:36:04 -0500

    inicio bd

---
+++ app/src/main/java/com/faroti/petshotel/model/database/PetDatabase.java
@@ -0,0 +1,10 @@
+package com.faroti.petshotel.model.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {}, version = 1)
+public abstract class PetDatabase extends RoomDatabase {
+
+
+}
---
+++ app/src/main/java/com/faroti/petshotel/model/database/entities/User.java
@@ -0,0 +1,53 @@
+package com.faroti.petshotel.model.database.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo(name = "NAME")
+    private String name;
+
+    @ColumnInfo(index = true)
+    private String email;
+
+    private String password;
+
+    private Boolean  enable;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+}
