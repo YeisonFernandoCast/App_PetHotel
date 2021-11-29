fdfec11acb3e9a9bcf86e4861dc1d26911911104
Johan <johanseb199@gmail.com>
2021-11-29 01:07:11 -0500

    config retrofit

---
+++ app/src/main/java/com/faroti/petshotel/model/http/RetrofitConfig.java
@@ -0,0 +1,29 @@
+package com.faroti.petshotel.model.http;
+
+import retrofit2.Retrofit;
+
+public class RetrofitConfig {
+
+    //singleton
+    private static RetrofitConfig instance;
+
+
+    public static RetrofitConfig getInstance() {
+        if(instance == null){
+            instance = new RetrofitConfig();
+        }
+        return instance;
+    }
+
+    private Retrofit retrofit;
+
+    private RetrofitConfig(){
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .build();
+    }
+
+    public Retrofit getRetrofit() {
+        return retrofit;
+    }
+}
