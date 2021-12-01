54761d70344c598b741e6b841327edfd7c0ddb90
Johan <johanseb199@gmail.com>
2021-11-30 21:38:27 -0500

    se cambio las direcciones de searchcontact

--- app/src/main/java/com/faroti/petshotel/model/SearchContactInteractor.java
+++ app/src/main/java/com/faroti/petshotel/model/SearchContactInteractor.java
@@ -2,7 +2,6 @@ package com.faroti.petshotel.model;
 
 import com.faroti.petshotel.mvp.SearchContactMVP;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,11 +11,11 @@ public class SearchContactInteractor implements SearchContactMVP.Model {
 
     public SearchContactInteractor(){
         data = Arrays.asList(
-                new SearchContactMVP.SearchContactInfo("Jose Pacheco", "Cra 23 # 45-67"),
-                new SearchContactMVP.SearchContactInfo("Johan Moscoso", "Cra 23 # 45-67"),
-                new SearchContactMVP.SearchContactInfo("Yeison Castaño", "Cra 23 # 45-67"),
-                new SearchContactMVP.SearchContactInfo("Rodrigo Estupiñán", "Cra 23 # 45-67"),
-                new SearchContactMVP.SearchContactInfo("Francisco J", "Cra 23 # 45-67")
+                new SearchContactMVP.SearchContactInfo("Jose Pacheco", "Cra 23AA # 8 - 79, Peñol, Colombia"),
+                new SearchContactMVP.SearchContactInfo("Johan Moscoso", "por la tienda de hernan eche para abajo en la casa con reja,Quindio, colombia  "),
+                new SearchContactMVP.SearchContactInfo("Yeison Castaño", "Cra 4B # 31A - 37 , Ibague, Colombia"),
+                new SearchContactMVP.SearchContactInfo("Rodrigo Estupiñán", "Cra 23 # 45-67, Buga laGrande, Colombia"),
+                new SearchContactMVP.SearchContactInfo("Francisco J", "Cra 23 # 45-67, Buga, Colombia")
         );
     }
 
